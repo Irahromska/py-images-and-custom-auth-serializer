e151dd85085417ec7ac241a4ba6fd7fc9b0dc092
Iryna Hromska <irahromska@gmail.com>
2025-09-19 09:28:37 +0300

    fix

--- user/admin.py
+++ user/admin.py
@@ -6,7 +6,10 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
-    """Define admin model for custom User model with no username field — email is the USERNAME_FIELD."""
+     """
+    Define admin model for custom User model with no username field.
+    Email is the USERNAME_FIELD.
+    """
 
     fieldsets = (
         (None, {"fields": ("email", "password")}),
